485dbc40a8803042f3753ab5104a76ef2907bc7c
Bartek Mogielnicki <64643049+bemog@users.noreply.github.com>
2021-09-16 09:26:50 +0200

    Update folders structure

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 
-import BottomTabNavigation from './navigation/BottomTabNavigation';
+import BottomTabNavigation from './src/navigation/BottomTabNavigation';
 
 const App = () => {
   return (
